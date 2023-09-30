a6c690f2d6f900371ed8cc5e429ca198a3bf019f
Clément M <cmt94.dev@gmail.com>
2023-09-30 16:51:46 +0200

    Render async home page w cars list from API

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 
-const Home = () => {
+import { fetchCars } from "@/utils";
+
+const Home = async () => {
+  const allCars = await fetchCars();
+  const isDataEmpty =
+    !Array.isArray(allCars) || allCars.length === 0 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -21,6 +27,21 @@ const Home = () => {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results.</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
