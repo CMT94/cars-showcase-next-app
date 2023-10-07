7d7ece9fb28adbb677e725346bef9947df456f08
Clément M <cmt94.dev@gmail.com>
2023-10-07 12:09:23 +0200

    handle error search input

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 
 import { SearchButton, SearchManufacturer } from ".";
 import SearchModel from "./SearchModel";
@@ -9,24 +8,36 @@ import SearchModel from "./SearchModel";
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = React.useState<string>("");
   const [model, setModel] = React.useState<string>("");
+  const [error, setError] = React.useState<string | undefined>(undefined);
 
-  const handleSearch = () => {};
+  const handleSearch = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
 
+    let err;
+    if (manufacturer === "" && model === "") {
+      err = "Please, fill in the search bar";
+      setError(err);
+    }
+  };
+
+  const updateSearchParams = () => {};
   return (
-    <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item">
-        <SearchManufacturer
-          manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
-        />
-        <SearchButton otherClasses="sm:hidden" />
-      </div>
-      <div className="searchbar__item">
-        <SearchModel model={model} setModel={setModel} />
-        <SearchButton otherClasses="sm:hidden" />
-      </div>
-      <SearchButton otherClasses="max-sm:hidden" />
-    </form>
+    <React.Fragment>
+      <form className="searchbar" onSubmit={handleSearch}>
+        <div className="searchbar__item">
+          <SearchManufacturer
+            manufacturer={manufacturer}
+            setManufacturer={setManufacturer}
+          />
+          <SearchButton otherClasses="sm:hidden" />
+        </div>
+        <div className="searchbar__item">
+          <SearchModel model={model} setModel={setModel} />
+          <SearchButton otherClasses="sm:hidden" />
+        </div>
+        <SearchButton otherClasses="max-sm:hidden" />
+      </form>
+    </React.Fragment>
   );
 };
 
