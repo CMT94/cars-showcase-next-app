7756f346f974a7d7b02e9170e35eed5c4fc9ef38
Clément M <cmt94.dev@gmail.com>
2023-10-07 18:43:33 +0200

    link searchParams in url with endpoint API fetchCars

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,14 @@ import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 
 import { fetchCars } from "@/utils";
 
-const Home = async () => {
-  const allCars = await fetchCars();
+const Home = async ({ searchParams }) => {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty =
     !Array.isArray(allCars) || allCars.length === 0 || !allCars;
@@ -17,7 +23,7 @@ const Home = async () => {
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Cars Catalogue</h1>
-          <p>Explore the cars you might like</p>
+          <p>Explore all the best cars you might like</p>
         </div>
 
         <div className="home__filters">
