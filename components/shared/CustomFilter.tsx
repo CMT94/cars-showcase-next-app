e199abd785f428a119e7b9c6dd8bfe79d274fd2e
Clément M <cmt94.dev@gmail.com>
2023-10-09 11:24:04 +0200

    handle update search params with btn filter

--- components/shared/CustomFilter.tsx
+++ components/shared/CustomFilter.tsx
@@ -7,17 +7,30 @@ import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 
 import { CustomFilterProps, OptionProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
+
   const [selected, setSelected] = React.useState(options[0]);
 
   const handleSelected = (e: OptionProps) => {
     setSelected(e);
   };
 
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    router.push(newPathName, { scroll: false });
+  };
+
+  const onChangeHandler = (e: OptionProps) => {
+    handleSelected(e);
+    handleUpdateParams(e);
+  };
+
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={handleSelected}>
+      <Listbox value={selected} onChange={onChangeHandler}>
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
